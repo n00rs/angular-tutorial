f7fd07a312f7d66ff5672b5faf492e69c18e1436
n00rs <007s.noorsha@gmail.com>
2024-01-13 13:54:52 +0530

    Angular 6 Malayalam - Create and use components v-7

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,MenuComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+name = 'JON';age = 22; rank= 1000
+
+}
