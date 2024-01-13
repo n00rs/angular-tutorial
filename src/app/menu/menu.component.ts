1302f9f9a691d3587099c1295850516570c52ed1
n00rs <007s.noorsha@gmail.com>
2024-01-13 17:42:08 +0530

    Routing in angular 6  tutorial  video 11

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Type } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
