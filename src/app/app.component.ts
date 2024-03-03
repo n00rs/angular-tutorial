8d5b188c99c75c4a07a78caaaab5fc15a6da7805
n00rs <007s.noorsha@gmail.com>
2024-03-03 15:30:50 +0530

    Angular Tutorials - Malayalam | Part -6 | Two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from "@angular/forms";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,36 +14,21 @@ export class AppComponent {
     console.log('hi from ngOnInit');
 
   }
-  public title = 'TITLE 9898';
-  data = { strTech1: 'Angular', strTech2: "Node Js", strPrgrmLanguage: 'Javascript' }
-  colorFunc() {
-    return 'blue'
-  }
-  public str = "H";
-  functionCall() {
-    console.log('HEy functionCall');
-    this.str = 'Good Morning'
-  }
-  functionCall2() {
-    console.log('HEy functionCall');
-    this.str = 'Good After Noon'
-  }
+
   public count = 0;
   public strOddOrEven = "Even"
-  increment() {
-    this.count++
-    this.strOddOrEven = this.doCheckOddEven(this.count)
-  }
-  decrement() {
-    if (this.count > 0)
-      this.count--;
-    this.strOddOrEven = this.doCheckOddEven(this.count)
+  blnHidden = true;
+  clear() {
+    this.count = 0;
+    this.strOddOrEven = "Even"
+    this.blnHidden = true;
   }
   doCheckOddEven(num: number) {
+    this.blnHidden = false;
+    this.strOddOrEven = num % 2 !== 0 ? "ODD" : "EVEN";
     return num % 2 !== 0 ? "ODD" : "EVEN";
   }
   onChange() {
     this.strOddOrEven = this.count % 2 !== 0 ? "ODD" : "EVEN";
-
   }
 }
