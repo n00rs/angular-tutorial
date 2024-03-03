a6aa567a43897d5aba3998a54201c93d879a9ea0
n00rs <007s.noorsha@gmail.com>
2024-03-03 14:48:23 +0530

    Angular Tutorials - Malayalam | Part -5 |Event Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,40 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  ngOnInit() {
+    console.log('hi from ngOnInit');
+
+  }
   public title = 'TITLE 9898';
   data = { strTech1: 'Angular', strTech2: "Node Js", strPrgrmLanguage: 'Javascript' }
-  colorFunc (){
+  colorFunc() {
     return 'blue'
+  }
+  public str = "H";
+  functionCall() {
+    console.log('HEy functionCall');
+    this.str = 'Good Morning'
+  }
+  functionCall2() {
+    console.log('HEy functionCall');
+    this.str = 'Good After Noon'
+  }
+  public count = 0;
+  public strOddOrEven = "Even"
+  increment() {
+    this.count++
+    this.strOddOrEven = this.doCheckOddEven(this.count)
+  }
+  decrement() {
+    if (this.count > 0)
+      this.count--;
+    this.strOddOrEven = this.doCheckOddEven(this.count)
+  }
+  doCheckOddEven(num: number) {
+    return num % 2 !== 0 ? "ODD" : "EVEN";
+  }
+  onChange() {
+    this.strOddOrEven = this.count % 2 !== 0 ? "ODD" : "EVEN";
+
   }
 }
