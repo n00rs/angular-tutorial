9d534a1371b6ec0c8ef1ba90bb0fa3466eaaeefa
n00rs <007s.noorsha@gmail.com>
2024-03-16 16:48:01 +0530

    Angular Tutorials - Malayalam | Part -11 | Reactive Forms

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.css'
 })
 export class AboutusComponent {
+  constructor(private fb: FormBuilder) {
 
+  }
+  submit = false
+  registraionForm = this.fb.group({
+    firstName: ["", Validators.required],
+    lastName: ["", Validators.required],
+    phone: ["", [Validators.required,Validators.pattern('[0-9]{10}')]],
+    email: ["", [Validators.required,Validators.email]]
+  });
+
+  get formContols() {
+    return this.registraionForm.controls;
+  }
+  onsubmit() {
+    this.submit = true
+    console.log('hello from ====>',this.formContols);
+    
+  }
 }
- 
